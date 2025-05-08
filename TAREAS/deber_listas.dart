c7971b1e55fa2a89edb79426abf4abbbc6bd3052
Vinicio1960m <vinicioaltamirano1969@gmail.com>
2025-05-08 11:39:06 -0500

    Con la Tarea

---
+++ TAREAS/deber_listas.dart
@@ -0,0 +1,9 @@
+
+// Listas
+void main() {
+  
+  List<String> clases = ['Lunes', 'Martes', 'Miercoles', 'Jueves'];
+  int numeroElementos= clases.length-1;
+  print('Numero de Elemento '+numeroElementos.toStringAsFixed(0)+' '+ clases[numeroElementos]);
+
+}
